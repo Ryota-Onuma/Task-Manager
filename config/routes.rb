bd52ed5863238cedcde8cf776129e0aa29ace847
Ryota Onuma <onumaryotajh@gmail.com>
2020-06-23 13:59:01 +0900

    必要なモデルの作成、及びアソシエーションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/signUp'
+  get 'users/signIn'
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20200623044418_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :content
+      t.integer :status
+      t.datetime :deadline
+      t.boolean :important
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
