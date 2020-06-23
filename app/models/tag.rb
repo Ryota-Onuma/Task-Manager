17f6de03dd29e4500e5eb8b900a61a72efd9b54d
Ryota Onuma <onumaryotajh@gmail.com>
2020-06-23 15:12:24 +0900

    rubocopで修正1

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Tag < ApplicationRecord
-    has_many :tag_tasks
-    has_many :tasks, through: :tag_tasks
+  has_many :tag_tasks
+  has_many :tasks, through: :tag_tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
   belongs_to :user
   has_many :tag_tasks, dependent: :destroy
   has_many :tags, through: :tag_tasks
-  
 end
