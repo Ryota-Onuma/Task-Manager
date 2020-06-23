58a93b27bbb675c082153f6005e4c739219d397c
Ryota Onuma <46987763+Ryota-Onuma@users.noreply.github.com>
2020-06-23 14:11:44 +0900

    Update routes.rb
    間違ってコントローラーを先に作っちゃったときの名残のルーティングが残っていたので消しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/signUp'
-  get 'users/signIn'
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
