02b9383ddd1fee66aeeacc2bd0f9526982ff8ae2
Ryota Onuma <46987763+Ryota-Onuma@users.noreply.github.com>
2020-07-14 10:26:25 +0900

    robocopでtasks_controller.rbを修正

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,46 +1,40 @@
 # frozen_string_literal: true
 
 class Api::TasksController < ApplicationController
-    def index
-        returnTasksAndUsersAllData
-       
-    end
+  def index
+    returnTasksAndUsersAllData
+  end
 
-    def create
-        task = Task.new(task_params)
-        task.deadline= DateTime.now
-        task.user_id = 1   #まだログイン機能をつけてないので
-        returnTasksAndUsersAllData　if task.save!
-    end
+  def create
+    task = Task.new(task_params)
+    task.deadline = DateTime.now
+    task.user_id = 1 # まだログイン機能をつけてないので
+    returnTasksAndUsersAllData if task.save!
+  end
 
-    def update
-         task = Task.find(params[:id])
-         returnTasksAndUsersAllData if task.update！(task_params)
-    end
+  def update
+    task = Task.find(params[:id])
+    task.title = params[:inputTask][:title]
+    task.deadline = params[:inputTask][:deadline]
+    task.content = params[:inputTask][:content]
+    task.status = params[:inputTask][:status]
+    task.important = params[:inputTask][:important]
+    returnTasksAndUsersAllData if task.save!
+  end
 
-    def destroy
-        task = Task.find(params[:id])
-        if task.destroy
-            returnTasksAndUsersAllData
-        else
-            render :json => {message: 'failed'}
-        end
-    end
+  private
 
+  def returnTasksAndUsersAllData
+    tasks = Task.all.order(created_at: 'DESC') # 新しい順
+    users = User.all
+    info = {
+      tasks: tasks,
+      users: users
+    }
+    render json: info
+  end
 
-    private
-    
-    def returnTasksAndUsersAllData
-        tasks = Task.all.order(created_at: 'DESC') #新しい順
-        users = User.all
-        info = {
-            tasks: tasks,
-            users:users
-        }
-        render :json => info
-    end
-
-    def task_params
-        params.require(:inputTask).permit(:id,:user_id,:created_at,:updated_at,:title,:content,:status,:deadline,:important)
-    end
+  def task_params
+    params.require(:inputTask).permit(:title, :content, :status, :deadline, :important)
+  end
 end
