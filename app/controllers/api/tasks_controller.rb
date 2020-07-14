99648ab030308f1755ff0225a0e5680819454e8f
Ryota Onuma <onumaryotajh@gmail.com>
2020-07-14 16:02:36 +0900

    Rubocopテスト

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -7,18 +7,18 @@ class Api::TasksController < ApplicationController
 
   def create
     task = Task.new(task_params)
-                           task.deadline = DateTime.now
+                                    task.deadline = DateTime.now
     task.user_id = 1 # まだログイン機能をつけてないので
-    returnTasksAndUsersAllData if task.save!
+                                    returnTasksAndUsersAllData if task.save!
   end
 
   def update
     task = Task.find(params[:id])
-                                            task.title = params[:inputTask][:title]
-                                         task.deadline = params[:inputTask][:deadline]
-    task.content = params[:inputTask][:content]
-                                
-                        task.status = params[:inputTask][:status]
+    task.title = params[:inputTask][:title]
+                                            task.deadline = params[:inputTask][:deadline]
+                                        task.content = params[:inputTask][:content]
+
+    task.status = params[:inputTask][:status]
     task.important = params[:inputTask][:important]
 
     returnTasksAndUsersAllData if task.save!
@@ -26,17 +26,17 @@ class Api::TasksController < ApplicationController
 
   private
 
-  def returnTasksAndUsersAllData
-    tasks = Task.all.order(created_at: 'DESC') # 新しい順
-    users = User.all
-    info = {
-      tasks: tasks,
-      users: users
-    }
-    render json: info
-  end
+    def returnTasksAndUsersAllData
+      tasks = Task.all.order(created_at: 'DESC') # 新しい順
+      users = User.all
+      info = {
+        tasks: tasks,
+        users: users
+      }
+      render json: info
+    end
 
-  def task_params
-    params.require(:inputTask).permit(:title, :content, :status, :deadline, :important)
-  end
+    def task_params
+      params.require(:inputTask).permit(:title, :content, :status, :deadline, :important)
+    end
 end
