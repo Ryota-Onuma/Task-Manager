f0ae7804e9bd3630b665676f4df92ddb522a3220
Ryota Onuma <onumaryotajh@gmail.com>
2020-07-14 15:58:04 +0900

    Rubocopテスト

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -7,18 +7,20 @@ class Api::TasksController < ApplicationController
 
   def create
     task = Task.new(task_params)
-    task.deadline = DateTime.now
+                           task.deadline = DateTime.now
     task.user_id = 1 # まだログイン機能をつけてないので
     returnTasksAndUsersAllData if task.save!
   end
 
   def update
     task = Task.find(params[:id])
-    task.title = params[:inputTask][:title]
-    task.deadline = params[:inputTask][:deadline]
+                                             task.title = params[:inputTask][:title]
+                                         task.deadline = params[:inputTask][:deadline]
     task.content = params[:inputTask][:content]
-    task.status = params[:inputTask][:status]
+                                
+                        task.status = params[:inputTask][:status]
     task.important = params[:inputTask][:important]
+    
     returnTasksAndUsersAllData if task.save!
   end
 
