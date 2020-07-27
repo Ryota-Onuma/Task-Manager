469917129f88642dac0083dd134a8cf88c1399d4
Ryota Onuma <onumaryotajh@gmail.com>
2020-07-27 13:35:10 +0900

    インデントを修正

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module App
     #　以下の記述を追記する(設定必須)
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
-   config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     config.generators.system_tests = nil
   end
 end
