50f944733d9e84428044b22f66789c3901d00008
Ryota Onuma <onumaryotajh@gmail.com>
2020-08-24 13:53:36 +0900

    fixed some rescue descriptions in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  rescue_from  ActiveRecord::RecordInvalid, with: :rescue_validation_422_error
-  rescue_from  ActiveRecord::RecordNotUnique, with: :rescue_validation_500_error
-  rescue_from  ActiveRecord::RecordNotSaved, with: :rescue_validation_422_error
-  rescue_from  ActiveRecord::StatementInvalid, with: :rescue_validation_500_error
-  rescue_from  ActiveRecord::RecordNotDestroyed,with: :rescue_validation_500_error
-  rescue_from  ActiveRecord::RecordNotFound, with: :rescue_validation_404_error   
+  rescue_from  ActiveRecord::RecordInvalid, with: :rescue_422_error
+  rescue_from  ActiveRecord::RecordNotUnique, with: :rescue_500_error
+  rescue_from  ActiveRecord::RecordNotSaved, with: :rescue_422_error
+  rescue_from  ActiveRecord::StatementInvalid, with: :rescue_500_error
+  rescue_from  ActiveRecord::RecordNotDestroyed, with: :rescue_500_error
+  rescue_from  ActiveRecord::RecordNotFound, with: :rescue_404_error
 
   private
 
-  def rescue_validation_422_error(error)
-    render status:422, json: { error: error }
+  def rescue_422_error(error)
+    render status: :unprocessable_entity, json: { error: error }
   end
-  def rescue_validation_500_error(error)
-    render status:500, json: { error: error }
+
+  def rescue_500_error(error)
+    render status: :internal_server_error, json: { error: error }
   end
-  def rescue_validation_404_error(error)
-    render status:404, json: { error: error }
+
+  def rescue_404_error(error)
+    render status: :not_found, json: { error: error }
   end
 end
