4a31f4a5e654f9192f112925401570220b22b386
Ryota Onuma <onumaryotajh@gmail.com>
2020-09-24 11:38:51 +0900

    modifed migration files

--- db/migrate/20200918062527_change_datatype_important_of_tasks.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDatatypeImportantOfTasks < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :tasks, :important
-    add_column :tasks, :important,:integer, default: 3 , null: false
-  end
-end
