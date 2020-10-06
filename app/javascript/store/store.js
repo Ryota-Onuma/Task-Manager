74a42343738f3ad79d6ac6cce709bf583ffe30ec
Ryota Onuma <onumaryotajh@gmail.com>
2020-10-06 10:45:32 +0900

    微調整

--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import createPersistedState from "vuex-persistedstate";
 import auth from "./modules/auth";
 import http from "./modules/http";
 import user from "./modules/user";
