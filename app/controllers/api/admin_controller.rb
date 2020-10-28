acb44ac01719db1fb7ed9e5547bb9cd801a9c313
Ryota Onuma <onumaryotajh@gmail.com>
2020-10-28 17:40:41 +0900

    mapを複数行にした

--- app/controllers/api/admin_controller.rb
+++ app/controllers/api/admin_controller.rb
@@ -4,7 +4,18 @@ class Api::AdminController < Api::ApplicationController
 
   def index
     users = User.preload(:tasks).all
-    users_tasks = users.map { |user| { "user": { name: user.name, email: user.email, introduction: user.introduction, admin: user.admin, permission: user.permission }, "tasks": user.tasks } }
+    users_tasks = users.map do |user|
+      {
+        "user": { 
+          name: user.name, 
+          email: user.email, 
+          introduction: user.introduction, 
+          admin: user.admin, 
+          permission: user.permission 
+          },
+        "tasks": user.tasks 
+      }
+    end
     render json: { users_tasks: users_tasks }
   end
 
