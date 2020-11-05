a2ce088f1f31b09f91ab0568e53c37e8b398224e
Ryota Onuma <onumaryotajh@gmail.com>
2020-11-05 15:15:34 +0900

    fix sytax

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,7 +1,7 @@
 class Api::SearchController < Api::ApplicationController
   before_action :authenticate!
   def todo_search
-    @q = Task.joins(:tagtasks).ransack(search_params)
+    @q = Task.joins(:tagtask).ransack(search_params)
     @tasks = @q.result(distinct: true)
     render json: { tasks: @tasks }
   end
