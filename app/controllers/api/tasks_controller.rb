50168413f03398949a1af0dfab89c7307b717b6b
Ryota Onuma <onumaryotajh@gmail.com>
2020-11-05 14:47:28 +0900

    tagのcrud処理

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -9,13 +9,29 @@ class Api::TasksController < Api::ApplicationController
   def create
     current_user = User.find_by(token: session[:token])
     task = Task.new(task_params)
+    tag_ids = params[:tags].map { |x| x.to_i }
+    tag_ids.each do |tag_id|
+      task.tag.create!(tag_id: tag_id)
+    end
     task.user_id = current_user.id
     task.save!
     returnTasksAndUsersAllData
   end
 
   def update
-    task = Task.find(params[:id])
+    task = Task.preload(:tagtasks).find(params[:id])
+    sent_tag_ids = params[:tags].map { |x| x.to_i }
+    old_tag_ids = task.tagtasks.map { |x| x.tag_id }
+
+    create_targets = sent_tag_ids - old_tag_ids　 # 更新により誕生したtaskたち
+    create_targets.each do |tag_id|
+      task.tagtask.create!(tag_id: tag_id)
+    end
+
+    delete_targets = old_tag_ids - sent_tag_ids # 更新により削除が運命づけられたtaskたち
+    delete_targets.each do |tag_id|
+      Tagtask.find_by(task_id: task.id, tag_id: tag_id).destroy!
+    end
     task.update!(task_params)
     returnTasksAndUsersAllData
   end
@@ -35,6 +51,7 @@ class Api::TasksController < Api::ApplicationController
     current_user = User.find_by(token: session[:token])
     tasks = Task.where(user_id: current_user.id).order(created_at: 'DESC') # 新しい順です
     tags = Tag.all
+    tagtasks = Tagtasks.all
     users = User.all
     info = {
       tasks: tasks,
