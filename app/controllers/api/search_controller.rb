bb19d6e73514ff5cda40239757939220771c8f5f
Ryota Onuma <onumaryotajh@gmail.com>
2020-11-05 15:34:48 +0900

    検索の仕組み構築

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,13 +1,18 @@
 class Api::SearchController < Api::ApplicationController
   before_action :authenticate!
   def todo_search
-    @q = Task.joins(:tagtask).ransack(search_params)
+    @q = Task.ransack(search_params)
     @tasks = @q.result(distinct: true)
+    if params[:tag_id].present?
+      @tasks.each_with_index do |task, index|
+        @tasks = @tasks.delete_at(index) unless task.tagtasks.map { |tagtask| tagtask.tag_id }.include?(params[:tag_id])
+      end
+    end
     render json: { tasks: @tasks }
   end
 end
 
 private
 def search_params
-  params.require(:q).permit(:title_cont, :status_eq, :important_eq, :tag_id_eq)
+  params.require(:q).permit(:title_cont, :status_eq, :important_eq)
 end
